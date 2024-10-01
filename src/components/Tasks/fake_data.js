d22f85122a97426d4ece0db9d008d9d422b10e8c
CuB1z <cub1zdev@gmail.com>
2024-10-01 20:32:50 +0100

    feat: Add addTask.js and fake_data.js to Tasks components

---
+++ src/components/Tasks/fake_data.js
@@ -0,0 +1,17 @@
+export const data = {
+    to_do: {
+        title: "To Do",
+        tasks: [
+            { id: 1, name: "Task 1" },
+            { id: 2, name: "Task 2" },
+        ],
+    },
+    ongoing: {
+        title: "Ongoing",
+        tasks: [{ id: 4, name: "Task 4" }],
+    },
+    completed: {
+        title: "Completed",
+        tasks: [{ id: 3, name: "Task 3" }],
+    },
+};
