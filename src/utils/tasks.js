e251f14da69e7c2f90349a466c7e27ca4d8f01d4
CuB1z <cub1zdev@gmail.com>
2024-10-03 08:45:13 +0200

    chore: Add uuid dependency and allow removing tasks

--- src/utils/tasks.js
+++ src/utils/tasks.js
@@ -1,6 +1,17 @@
+import { v4 as uuid } from 'uuid'
+
+/**
+ * Add a new task to the tasks array of a specific key.
+ * 
+ * @param {object} data
+ * @param {string} key 
+ * @param {string} name 
+ * @param {string} tag 
+ * @returns {{ ok: boolean, data: object }}
+ */
 function addTask(data, key, name, tag) {
     const newTask = {
-        id: Date.now(),
+        id: uuid(),
         tag: tag ? tag : "AD",
         name: name ? name : "Provide a task name"
     }
@@ -15,4 +26,27 @@ function addTask(data, key, name, tag) {
     }
 }
 
-export { addTask }
+/**
+ * Remove a task from the tasks array of a specific key.
+ * 
+ * @param {object} data
+ * @param {string} key
+ * @param {string} taskId
+ * @returns {{ ok: boolean, data: object }}
+ */
+function removeTask(data, key, taskId) {
+    try {
+        const updatedTasks = data[key].tasks.filter(task => task.id !== taskId)
+        data[key].tasks = updatedTasks
+        const updatedData = { ...data }
+        return { ok: true, data: updatedData }
+    } catch (error) {
+        console.error(error)
+        return { ok: false, data }
+    }
+}
+
+export {
+    addTask,
+    removeTask
+}
