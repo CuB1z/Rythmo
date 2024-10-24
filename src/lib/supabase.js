4f8ab8dcd3779548e5a7467eada4bc4a87b2db97
CuB1z <cub1zdev@gmail.com>
2024-10-24 20:01:12 +0200

    chore: Add login functionality (to improve UI)

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -5,11 +5,34 @@ export const supabase = createClient(
     import.meta.env.SUPABASE_KEY,
 );
 
+/**
+ * Register a new user with email and password in Supabase
+ * 
+ * @param {string} email 
+ * @param {string} password 
+ * @returns {Promise<{data: any, error: any}>}
+ */
 export const registerUser = async (email, password) => {
     const { data, error } = await supabase.auth.signUp({
         email: email,
         password: password,
     })
     
+    return { data, error }
+}
+
+/**
+ * Login a user with email and password in Supabase
+ * 
+ * @param {string} email
+ * @param {string} password
+ * @returns {Promise<{data: any, error: any}>}
+*/
+export const loginUser = async (email, password) => {
+    const { data, error } = await supabase.auth.signInWithPassword({
+        email: email,
+        password: password,
+    })
+
     return { data, error }
 }
---
+++ src/pages/api/login.js
@@ -0,0 +1,25 @@
+import { loginUser } from "@lib/supabase"
+
+export const POST = async ({ request }) => {
+    // Get the email and password from the request body
+    const { email, password } = await request.json()
+
+    // Register the user
+    const { error } = await loginUser(email, password)
+
+    // If there's an error, return the error
+    if (error) {
+        return new Response(JSON.stringify({
+            ok: false,
+            message: error.message,
+            status: 400,
+        }))
+    }
+
+    // If there's no error, return a success response
+    return new Response(JSON.stringify({
+        ok: true,
+        message: "User logged in",
+        status: 200,
+    }))
+}
