845a9659c54b90f560d24b6e1f4136b87df8a5c1
CuB1z <cub1zdev@gmail.com>
2024-12-08 20:44:04 +0000

    vercel: Add vercel config for deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-
 import vue from '@astrojs/vue';
+import vercelServerless from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), vue()],
   output: "server",
+  adapter: vercelServerless(),
   image: {
     remotePatterns: [{ protocol: 'https' }]
   }
